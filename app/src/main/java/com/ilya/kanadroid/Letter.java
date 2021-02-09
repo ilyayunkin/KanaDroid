a6fd271f7cfdb291b0014357ee75dc675fce9572
ilyayunkin <ilya.yunkin@ya.ru>
2021-02-10 02:55:00 +0600

    Changing letters set implemented

---
+++ app/src/main/java/com/ilya/kanadroid/Letter.java
@@ -0,0 +1,11 @@
+package com.ilya.kanadroid;
+
+public class Letter{
+    String letter;
+    String reading;
+
+    public Letter(String letter, String reading) {
+        this.letter = letter;
+        this.reading = reading;
+    }
+}
--- app/src/main/java/com/ilya/kanadroid/MainActivity.java
+++ app/src/main/java/com/ilya/kanadroid/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -17,6 +18,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView textView;
     TextView rightView;
     TextView wrongView;
+
+    final Random random = new Random();
+    Letter[] portion= new Letter[5];
+    Letter currentLetter;
+    int position = 0;
+    int right = 0;
+    int wrong = 0;
+    int currentIndex = 0;
+    boolean[] rightLetter = new boolean[5];
 
     final Letter[] hiragana ={
             new Letter("あ", "a"),
@@ -76,10 +86,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             new Letter("ん", "n"),
     };
-    final Random random = new Random();
-    Letter currentLetter;
-    int right = 0;
-    int wrong = 0;
 
     @Override
     public void onClick(View v) {
@@ -87,25 +93,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String answer = b.getText().toString();
         String rightAnswer = currentLetter.reading;
         if(answer.equals(rightAnswer)){
-            updateLetter();
             Toast.makeText(this , "Right!", Toast.LENGTH_SHORT).show();
             ++right;
+            rightLetter[currentIndex] = true;
             rightView.setText(String.valueOf(right));
+
+            System.out.println("Answers: " + Arrays.toString(rightLetter));
+            boolean allTrue = true;
+            for(int i = 0; i < 5; ++i) {
+                if(rightLetter[i] == false) {
+                    allTrue = false;
+                }
+            }
+
+            if(allTrue){
+                System.out.println("Update level");
+                updateLevel();
+            }else{
+                System.out.println("Update letters");
+                updateLetter();
+            }
         }else{
             updateLetter();
             Toast.makeText(this , "Wrong!", Toast.LENGTH_SHORT).show();
             ++wrong;
             wrongView.setText(String.valueOf(wrong));
-        }
-    }
-
-    public class Letter{
-        String letter;
-        String reading;
-
-        public Letter(String letter, String reading) {
-            this.letter = letter;
-            this.reading = reading;
+            for(int i = 0; i < 5; ++i){
+                rightLetter[i] = false;
+            }
         }
     }
 
@@ -124,18 +139,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         for(int i = 0; i < 5; ++i){
             buttons.get(i).setOnClickListener(this);
         }
-        updateLetter();
+        loadLetters();
     }
     void updateLetter(){
         Letter oldLetter = currentLetter;
-        Letter newLetter = hiragana[random.nextInt(5)];
+        currentIndex = random.nextInt(5);
+        Letter newLetter = portion[currentIndex];
+
         while(newLetter == oldLetter){
-            newLetter = hiragana[random.nextInt(5)];
+            currentIndex = random.nextInt(5);
+            newLetter = portion[currentIndex];
         };
+
         currentLetter = newLetter;
         textView.setText(currentLetter.letter);
         for(int i = 0; i < 5; ++i){
-            buttons.get(i).setText(hiragana[i].reading);
+            buttons.get(i).setText(portion[i].reading);
         }
+    }
+    void updateLevel(){
+        if(position == 0)
+            position+= 5;
+        else
+            position+= 3;
+        loadLetters();
+    }
+    void loadLetters(){
+        if(position == 0){
+            for(int i = 0; i < 5; ++i){
+                portion[i] = hiragana[position + i];
+            }
+        }else{
+            for(int i = 0; i < 3; ++i){
+                portion[i] = hiragana[position + i];
+            }
+            portion[3] = hiragana[random.nextInt(position)];
+            portion[4] = hiragana[random.nextInt(position)];
+        }
+        for(int i = 0; i < 5; ++i){
+            rightLetter[i] = false;
+        }
+        updateLetter();
     }
 }
