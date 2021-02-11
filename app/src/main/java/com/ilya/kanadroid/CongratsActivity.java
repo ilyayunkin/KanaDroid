2066a212f304ab85da5dd388385810648b6ebdcc
ilyayunkin <ilya.yunkin@ya.ru>
2021-02-11 22:40:36 +0600

    OK button added

--- app/src/main/java/com/ilya/kanadroid/CongratsActivity.java
+++ app/src/main/java/com/ilya/kanadroid/CongratsActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class CongratsActivity extends AppCompatActivity {
+
+    public void onOkClicked(View v) {
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
